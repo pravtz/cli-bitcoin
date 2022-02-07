80f6595b6dfa344815fbb70e6ea250f86e613927
pravtz <ederson.ufrgs@gmail.com>
2022-02-07 11:35:05 -0300

    fix main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,170 @@
 #!/usr/bin/env node
+const inquirer = require('inquirer');
 const { program } = require('commander');
 
 const pkg = require('../package.json');
 const Service = require('./service');
 
+const service = new Service();
+
+const listCoin = ['btc', 'eth'];
+const listMethod = ['ticker', 'orderbook', 'trades', 'day-summary'];
+
+const question = [
+  {
+    type: 'list', name: 'coin', message: 'Escolha a criptomoeda', choices: listCoin,
+  },
+  {
+    type: 'list', name: 'method', message: 'Escolha o metodo', choices: listMethod,
+  },
+  {
+    type: 'confirm', name: 'output', message: 'Saída em JSON?',
+  },
+
+];
+
+const order = () => {
+  inquirer
+    .prompt(question)
+    .then((answers) => {
+      service.getDefault(answers.coin, answers.method)
+        .then(
+          answers.output ? console.log : console.table,
+        );
+    });
+};
+
 program
   .version(pkg.version)
   .description(pkg.description)
-  .option('-c, --coin [string]', 'Digital currency acronym | Acrônimo da moeda digital [coin] ')
-  .option('-m, --method [string] ', 'Método da API de Dados requisitado - " ticker, orderbook, trades "')
-  .parse(process.argv);
+  .action(() => {
+    order();
+  })
+  .addHelpText('after', `
+  Examples:
+    $ cli-bitcoin
+    $ cli-bitcoin --help
 
-const data = new Service();
-const options = program.opts();
-data.getDefault(options.coin, options.method).then(console.log);
+    $ cli-bitcoin tinker
+    $ cli-bitcoin tinker -c btc
+    $ cli-bitcoin tinker --help
+
+    $ cli-bitcoin trades
+    $ cli-bitcoin trades --help
+
+    $ cli-bitcoin orderbook
+    $ cli-bitcoin orderbook --help
+
+    $ cli-bitcoin day-summary
+    $ cli-bitcoin day-summary --help
+  `);
+
+program
+  .command('ticker [string]')
+  .description('Retorna informações com o resumo das últimas 24 horas de negociações.')
+  .option('-c, --coin [coin]', 'Acrônimo da moeda digital [coin] ')
+  .option('-o --output [output]', 'output in the table') // json, table
+  .option('-hd --human-date ', 'output human-readable date')
+  .option('-hc --human-currency ', 'output human-readable currency')
+  .action((test, options, command) => {
+    const chosenOutput = command.opts().output === 'json' || command.opts().output === 'table' ? 'json' : 'text';
+    return service.getTicker(
+      command.opts().coin,
+      command.opts().humanDate,
+      command.opts().humanCurrency,
+      chosenOutput,
+    ).then(command.opts().output === 'table' ? console.table : console.log);
+  })
+  .addHelpText('after', `
+  Examples:
+    $ cli-bitcoin ticker
+    $ cli-bitcoin ticker -c eth`);
+
+program
+  .command('orderbook [string]')
+  .description('Livro de ofertas é composto por duas listas: (1) uma lista com as ofertas de compras ordenadas pelo maior valor; (2) uma lista com as ofertas de venda ordenadas pelo menor valor.')
+  .option('-c, --coin [coin]', 'Acrônimo da moeda digital [coin] ')
+  .option('-o, --output [output]', 'choise output| text <default> [json, table] ')
+  .option('-hd --human-date ', 'output human-readable date')
+  .option('-hc --human-currency ', 'output human-readable currency')
+  .option('-of --offers [offers]', 'filter data | options are: "asks", "dids" and "full"')
+  .action((test, options, command) => {
+    const chosenOutput = command.opts().output === 'json' || command.opts().output === 'table' ? 'json' : 'text';
+    return service.getOrderbook(
+      command.opts().coin,
+      command.opts().humanDate,
+      command.opts().humanCurrency,
+      chosenOutput,
+      command.opts().offers,
+    )
+      .then(command.opts().output === 'table' ? console.table : console.log);
+  })
+  .addHelpText('after', `
+  Examples:
+  Livro de ofertas é composto por duas listas: (1) uma lista com as ofertas de compras ordenadas pelo maior valor; (2) uma lista com as ofertas de venda ordenadas pelo menor valor. O livro mostra até 1000 ofertas de compra e até 1000 ofertas de venda.
+
+  Uma oferta é constituída por uma ou mais ordens, sendo assim, a quantidade da oferta é o resultado da soma das quantidades das ordens de mesmo preço unitário. Caso uma oferta represente mais de uma ordem, a prioridade de execução se dá com base na data de criação da ordem, da mais antiga para a mais nova.
+
+    $ cli-bitcoin orderbook
+    $ cli-bitcoin orderbook -c eth
+    $ cli-bitcoin orderbook --help
+
+  `);
+program
+  .command('trades [string]')
+  .description('Histórico de negociações realizadas.')
+  .option('-C, --coin [coin]', 'Acrônimo da moeda digital [coin] ')
+  .option('-t, --tid [tid]', 'Tempo | Retorna até 1000 negociações a partir do identificador da negociação ')
+  .option('-s, --since [since]', 'Desde | Retorna até 1000 negociações a partir do identificador da negociação ')
+  .option('-f, --from [from]', 'Retorna até 1000 negociações a partir da data informada')
+  .option('-ft, --fron-to [fromTo]', 'Retorna até 1000 negociações entre o intervalo de timestamp informado.')
+  .option('-hd --human-date ', 'output human-readable date')
+  .option('-hc --human-currency ', 'output human-readable currency')
+  .action((test, options, command) => {
+    const result = service.getTrades(
+      command.opts().from,
+      command.opts().fronTo,
+      command.opts().tid,
+      command.opts().since,
+      command.opts().coin,
+      command.opts().humanDate,
+      command.opts().humanCurrency,
+    );
+    return result.then(console.log);
+  })
+  .addHelpText('after', `
+  Examples:
+
+    $ cli-bitcoin trades
+    $ cli-bitcoin trades -c eth
+    $ cli-bitcoin trades -t 5700 -c eth
+    $ cli-bitcoin trades -s 5700 -c eth
+    $ cli-bitcoin trades -f 1501871369
+    $ cli-bitcoin trades -ft 1501871369 1501891200
+    $ cli-bitcoin trades --help
+
+  `);
+
+program
+  .command('day-summary [string]')
+  .description('Resumo diário de negociações realizadas')
+  .option('-C, --coin [coin]', 'Acrônimo da moeda digital [coin] ')
+  .option('-d, --coin [data]', 'data')
+
+  .action((test, options, command) => {
+    console.log(command);
+    console.log(`Called '${command.name()}'`);
+    console.log(`args: ${command.args}`);
+    console.log('opts: %o', command.opts());
+  })
+  .addHelpText('after', `
+  Examples:
+
+    $ cli-bitcoin day-summary
+    $ cli-bitcoin day-summary -c eth
+    $ cli-bitcoin trades -d 2020 11 15
+    $ cli-bitcoin trades --help
+
+  `);
+
+program.parse(process.argv);
--- tests/e2e/main.test.js
+++ tests/e2e/main.test.js
@@ -3,24 +3,81 @@ const { describe, it } = require('mocha');
 const { exec } = require('child_process');
 const pkg = require('../../package.json');
 
-const converterBtc = './src/main.js';
+const cliBitcoin = './src/main.js';
 
 describe('Main - Tests Integrations', () => {
-  it('should retorn the version when comander converterBtc --version', (done) => {
-    exec(`${converterBtc} --version`, (err, stdout) => {
-      if (err) throw err;
-      expect(stdout.replace('\n', '')).to.be.equal('1.0.0');
-      done();
+  context('Default', () => {
+    it('should retorn the version when comander cli-bitcoin --version', (done) => {
+      exec(`${cliBitcoin} --version`, (err, stdout) => {
+        if (err) throw err;
+        expect(stdout.replace('\n', '')).to.be.equal('1.0.0');
+        done();
+      });
+    });
+
+    it('should retorn the description when comander cli-bitcoin --help', (done) => {
+      exec(`${cliBitcoin} --help`, (err, stdout) => {
+        if (err) throw err;
+
+        expect(stdout.includes(pkg.description)).to.be.true;
+
+        done();
+      });
     });
   });
+  context('Command Ticker', () => {
+    it('should retorn the helper of the ticker when comander cli-bitcoin ticker --help', (done) => {
+      exec(`${cliBitcoin} ticker --help`, (err, stdout) => {
+        if (err) throw err;
+        const expected = 'Retorna informações com o resumo das últimas 24 horas de negociações.';
+        const expected2 = '$ cli-bitcoin ticker';
+        expect(stdout.includes(expected)).to.be.true;
+        expect(stdout.includes(expected2)).to.be.true;
 
-  it('should retorn the description when comander converterBtc --help', (done) => {
-    exec(`${converterBtc} --help`, (err, stdout) => {
-      if (err) throw err;
-
-      expect(stdout.includes(pkg.description)).to.be.true;
-
-      done();
+        done();
+      });
+    });
+    it.skip('should retorn data tinker when commander cli-bitcoin tinker -c btc', (done) => {
+      exec(`${cliBitcoin} ticker`, (err, stdout) => {
+        if (err) throw err;
+        const expected = 'high';
+        const stringStdout = JSON.stringify(stdout);
+        expect(stringStdout.includes(expected)).to.be.true;
+        done();
+      });
+    });
+  });
+  context('Command Orderbook', () => {
+    it.skip('should retorn data tinker when commander cli-bitcoin orderbook', (done) => {
+      exec(`${cliBitcoin} orderbook`, (err, stdout) => {
+        const expected = '';
+        if (err) throw err;
+        const stringStdout = JSON.stringify(stdout);
+        expect(stringStdout.includes(expected)).to.be.true;
+        done();
+      });
+    });
+  });
+  context('Command Trader', () => {
+    it.skip('should retorn data tinker when commander cli-bitcoin trader', (done) => {
+      exec(`${cliBitcoin} trader`, (err, stdout) => {
+        const expected = '';
+        if (err) throw err;
+        const stringStdout = JSON.stringify(stdout);
+        expect(stringStdout.includes(expected)).to.be.true;
+        done();
+      });
+    });
+  });
+  context('Command daySummary', () => {
+    it.skip('should retorn data tinker when commander cli-bitcoin daySummary 07 11 2020', (done) => {
+      exec(`${cliBitcoin} daySummary 07 11 2020`, (err, stdout) => {
+        const expected = '2020-11-07';
+        if (err) throw err;
+        const stringStdout = JSON.stringify(stdout);
+        expect(stringStdout.includes(expected)).to.be.true;
+        done();
+      });
     });
   });
 });
