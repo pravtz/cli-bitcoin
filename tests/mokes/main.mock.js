a05e5ee84685d4900b7606fe0ad4a6ef2c52dbde
pravtz <ederson.ufrgs@gmail.com>
2022-02-07 11:29:19 -0300

    add moke

--- tests/mokes/main.mock.js
+++ tests/mokes/main.mock.js
@@ -4,6 +4,7 @@ const mokeTrades = require('./valid-trades.json');
 const mokeOrderbook = require('./valid-orderbook.json');
 const mokeSaySummary = require('./valid-day-summary.json');
 const moketradesCustonFromAndFromTo = require('./valid-trades-custon-from-and-from-to.json');
+const moketradesCustonFrom = require('./valid-trades-custon-from.json');
 const moketradesCustonTid = require('./valied-trades-custon-tid.json');
 const moketradesCustonSince = require('./valied-trades-custon-since.json');
 const Repository = require('../../src/repository');
@@ -16,14 +17,16 @@ const mock = {
   moketradesCustonFromAndFromTo,
   moketradesCustonTid,
   moketradesCustonSince,
+  moketradesCustonFrom,
 };
 
 const url = {
-  ticker: 'https://www.mercadobitcoin.net/api/btc/ticker/',
-  trades: 'https://www.mercadobitcoin.net/api/btc/trades/',
-  orderbook: 'https://www.mercadobitcoin.net/api/btc/orderbook/',
+  ticker: 'https://www.mercadobitcoin.net/api/btc/ticker',
+  trades: 'https://www.mercadobitcoin.net/api/btc/trades',
+  orderbook: 'https://www.mercadobitcoin.net/api/btc/orderbook',
   daySummary: 'https://www.mercadobitcoin.net/api/btc/day-summary/2013/06/20/',
   tradesCustonFromAndFromTo: 'https://www.mercadobitcoin.net/api/btc/trades/1643020542/1643020552',
+  tradesCustonFrom: 'https://www.mercadobitcoin.net/api/btc/trades/1643020542',
   tradesCustonTid: 'https://www.mercadobitcoin.net/api/btc/trades/?tid=12930657',
   tradesCustonSince: 'https://www.mercadobitcoin.net/api/btc/trades/?since=12930657',
 };
@@ -36,6 +39,7 @@ stub.withArgs(url.trades).resolves(mock.mokeTrades);
 stub.withArgs(url.orderbook).resolves(mock.mokeOrderbook);
 stub.withArgs(url.daySummary).resolves(mock.mokeSaySummary);
 stub.withArgs(url.tradesCustonFromAndFromTo).resolves(mock.moketradesCustonFromAndFromTo);
+stub.withArgs(url.tradesCustonFrom).resolves(mock.moketradesCustonFrom);
 stub.withArgs(url.tradesCustonTid).resolves(mock.moketradesCustonTid);
 stub.withArgs(url.tradesCustonSince).resolves(mock.moketradesCustonSince);
 
